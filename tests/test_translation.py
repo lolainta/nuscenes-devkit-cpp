d7b4e3f1a2b343389d40052a90d1296efffaafac
LolaInTa Chi <lolainta@gmail.com>
2023-12-11 22:46:04 +0800

    test: tarnsform test

---
+++ tests/test_translation.py
@@ -0,0 +1,15 @@
+import _nuscenes as nuscenes
+import random
+
+
+def test_translation():
+    nusc = nuscenes.Nuscenes(path="./data", version="v1.0-mini", verbose=False)
+    assert isinstance(nusc.ego_positions, list)
+    assert len(nusc.ego_positions) != 0
+    ego_pose = random.sample(nusc.ego_positions, 1)[0]
+    assert isinstance(ego_pose, nuscenes.EgoPosition)
+    trans = ego_pose.translation
+    assert isinstance(trans, nuscenes.Translation)
+    assert isinstance(trans.x, float)
+    assert isinstance(trans.y, float)
+    assert isinstance(trans.z, float)
