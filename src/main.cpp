95dcdfd18ae3ca9a3dbd4200ae797f8e46f012bc
LolaInTa Chi <lolainta@gmail.com>
2023-12-11 14:37:29 +0800

    build: cmake with pybind11 & pytest

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <pybind11/pybind11.h>
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_nuscenes, m) {
+#ifdef VERSION_INFO
+  m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+  m.attr("__version__") = "dev";
+#endif
+}
